89bf9ac318d2d46206fcee64378222cf735aa1d3
Rodrigo Fernandes <rfernandes@gmx.com>
2019-06-21 18:21:55 +0100

    GH-14 Added convenience headers
    Result of running:
    
    ```
    for dir in *; do [[ -d $dir ]] && ls $dir/*.hpp | { head -n 16 io/Buffer.hpp; echo -e "\n#ifndef TOOLBOX_${dir^^}_HPP\n#define TOOLBOX_${dir^^}_HPP\n"; sed 's/.*/#include "&"/'; echo -e "\n#endif // TOOLBOX_${dir^^}_HPP"; } > $dir.hpp ; done
    ```

---
+++ toolbox/io.hpp
@@ -0,0 +1,35 @@
+// The Reactive C++ Toolbox.
+// Copyright (C) 2013-2019 Swirly Cloud Limited
+// Copyright (C) 2019 Reactive Markets Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef TOOLBOX_IO_HPP
+#define TOOLBOX_IO_HPP
+
+#include "io/Buffer.hpp"
+#include "io/Epoll.hpp"
+#include "io/EpollReactor.hpp"
+#include "io/EventFd.hpp"
+#include "io/Event.hpp"
+#include "io/File.hpp"
+#include "io/Handle.hpp"
+#include "io/Interruptible.hpp"
+#include "io/Muxer.hpp"
+#include "io/Reactor.hpp"
+#include "io/Runner.hpp"
+#include "io/TimerFd.hpp"
+#include "io/Timer.hpp"
+
+#endif // TOOLBOX_IO_HPP
---
+++ toolbox/net.hpp
@@ -0,0 +1,35 @@
+// The Reactive C++ Toolbox.
+// Copyright (C) 2013-2019 Swirly Cloud Limited
+// Copyright (C) 2019 Reactive Markets Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef TOOLBOX_NET_HPP
+#define TOOLBOX_NET_HPP
+
+#include "net/DgramSock.hpp"
+#include "net/Endpoint.hpp"
+#include "net/Error.hpp"
+#include "net/IoSock.hpp"
+#include "net/IpAddr.hpp"
+#include "net/McastSock.hpp"
+#include "net/Protocol.hpp"
+#include "net/Resolver.hpp"
+#include "net/Runner.hpp"
+#include "net/Socket.hpp"
+#include "net/StreamAcceptor.hpp"
+#include "net/StreamConnector.hpp"
+#include "net/StreamSock.hpp"
+
+#endif // TOOLBOX_NET_HPP
