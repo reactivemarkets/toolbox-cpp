42af8e3b0b04be3200afa2d56e69d98ffe61aee3
Rodrigo Fernandes <rfernandes@gmx.com>
2019-06-21 18:21:55 +0100

    GH-14 Added convenience headers
    Result of running:
    
    ```
    for dir in *; do [[ -d $dir ]] && ls $dir/*.hpp | { head -n 16 io/Buffer.hpp; echo -e "\n#ifndef TOOLBOX_${dir^^}_HPP\n#define TOOLBOX_${dir^^}_HPP\n"; sed 's/.*/#include "&"/'; echo -e "\n#endif // TOOLBOX_${dir^^}_HPP"; } > $dir.hpp ; done
    ```

---
+++ toolbox/util.hpp
@@ -0,0 +1,45 @@
+// The Reactive C++ Toolbox.
+// Copyright (C) 2013-2019 Swirly Cloud Limited
+// Copyright (C) 2019 Reactive Markets Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef TOOLBOX_UTIL_HPP
+#define TOOLBOX_UTIL_HPP
+
+#include "util/Array.hpp"
+#include "util/Compare.hpp"
+#include "util/Config.hpp"
+#include "util/Enum.hpp"
+#include "util/Exception.hpp"
+#include "util/Finally.hpp"
+#include "util/IntTypes.hpp"
+#include "util/Math.hpp"
+#include "util/RefCount.hpp"
+#include "util/RingBuffer.hpp"
+#include "util/Ryu.hpp"
+#include "util/Slot.hpp"
+#include "util/Stream.hpp"
+#include "util/StringBuf.hpp"
+#include "util/String.hpp"
+#include "util/Struct.hpp"
+#include "util/Tokeniser.hpp"
+#include "util/Traits.hpp"
+#include "util/TypeTraits.hpp"
+#include "util/Utility.hpp"
+#include "util/Variant.hpp"
+#include "util/VarSub.hpp"
+#include "util/Version.hpp"
+
+#endif // TOOLBOX_UTIL_HPP
