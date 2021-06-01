a0d18786085ed36b93e6babf00ccf085e303444c
markaylett <markaylett@users.noreply.github.com>
2021-06-01 06:58:34 +0000

    deploy: 555e77a2ee69e8b0ca3f5c38ece673352609ad6a

--- search/classes_c.js
+++ search/classes_c.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['options_1207',['Options',['../classtoolbox_1_1util_1_1Options.html',1,'toolbox::Options'],['../classtoolbox_1_1util_1_1Options.html',1,'toolbox::util::Options']]],
   ['ostaticstream_1208',['OStaticStream',['../classtoolbox_1_1util_1_1OStaticStream.html',1,'toolbox::OStaticStream&lt; MaxN &gt;'],['../classtoolbox_1_1util_1_1OStaticStream.html',1,'toolbox::util::OStaticStream&lt; MaxN &gt;']]],
-  ['ostream_1209',['OStream',['../classtoolbox_1_1http_1_1OStream.html',1,'toolbox::http::OStream'],['../classtoolbox_1_1http_1_1OStream.html',1,'toolbox::OStream'],['../classtoolbox_1_1util_1_1OStream.html',1,'toolbox::util::OStream&lt; MaxN &gt;']]],
+  ['ostream_1209',['OStream',['../classtoolbox_1_1http_1_1OStream.html',1,'toolbox::http::OStream'],['../classtoolbox_1_1io_1_1OStream.html',1,'toolbox::io::OStream'],['../classtoolbox_1_1http_1_1OStream.html',1,'toolbox::OStream'],['../classtoolbox_1_1util_1_1OStream.html',1,'toolbox::util::OStream&lt; MaxN &gt;']]],
   ['overloaded_1210',['overloaded',['../structtoolbox_1_1util_1_1overloaded.html',1,'toolbox::overloaded&lt; Ts &gt;'],['../structtoolbox_1_1util_1_1overloaded.html',1,'toolbox::util::overloaded&lt; Ts &gt;']]]
 ];
--- search/functions_16.js
+++ search/functions_16.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['xsputn_2020',['xsputn',['../classtoolbox_1_1http_1_1StreamBuf.html#af339cc66e490111f27110e7c25d66820',1,'toolbox::http::StreamBuf']]]
+  ['xsputn_2020',['xsputn',['../classtoolbox_1_1http_1_1StreamBuf.html#ac8dd5ac7024053b289c634e6133e35d3',1,'toolbox::http::StreamBuf::xsputn()'],['../classtoolbox_1_1io_1_1StreamBuf.html#a7971ae8b46c79bd31262817f0c179d90',1,'toolbox::io::StreamBuf::xsputn()']]]
 ];
