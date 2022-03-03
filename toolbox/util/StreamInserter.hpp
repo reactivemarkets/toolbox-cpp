f25fd8ea35f5615f582628c33b411d51e30bca6c
Mark Aylett <mark.aylett@reactivemarkets.com>
2022-03-03 07:46:34 +0000

    feat(util): add stream inserter template
    StreamInserter is a class template designed to simplify I/O
    manipulator and stream inserter implementations by reducing
    boiler-plate code.
    
    SDB-2980

---
+++ toolbox/util/StreamInserter.hpp
@@ -0,0 +1,62 @@
+// The Reactive C++ Toolbox.
+// Copyright (C) 2022 Reactive Markets Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TOOLBOX_UTIL_STREAMINSERTER_HPP
+#define TOOLBOX_UTIL_STREAMINSERTER_HPP
+
+#include <concepts>
+#include <iosfwd>
+#include <utility>
+
+namespace toolbox {
+inline namespace util {
+
+/// StreamInserter is a class template designed to simplify I/O manipulator and stream inserter
+/// implementations by reducing boiler-plate code.
+///
+// clang-format off
+template <typename FnT>
+requires std::invocable<FnT, std::ostream&>
+// clang-format on
+class StreamInserter {
+    friend std::ostream& operator<<(std::ostream& os, const StreamInserter& si)
+    {
+        si.fn_(os);
+        return os;
+    }
+
+  public:
+    explicit StreamInserter(FnT fn)
+    : fn_{std::move(fn)}
+    {
+    }
+    ~StreamInserter() = default;
+
+    // Copy.
+    StreamInserter(const StreamInserter&) = delete;
+    StreamInserter& operator=(const StreamInserter&) = delete;
+
+    // Move.
+    StreamInserter(StreamInserter&&) noexcept = default;
+    StreamInserter& operator=(StreamInserter&&) noexcept = delete;
+
+  private:
+    FnT fn_;
+};
+
+} // namespace util
+} // namespace toolbox
+
+#endif // TOOLBOX_UTIL_STREAMINSERTER_HPP
