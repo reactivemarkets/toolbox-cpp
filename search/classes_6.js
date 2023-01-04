788e14a93f963c3b508d12aeeaf6a29711448142
MarkJamesStevenson <MarkJamesStevenson@users.noreply.github.com>
2023-01-04 13:40:26 +0000

    deploy: db6462888ae6e46a02daf91ee04af9c718f6f352

--- search/classes_6.js
+++ search/classes_6.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['handle_0',['Handle',['../classtoolbox_1_1io_1_1Reactor_1_1Handle.html',1,'toolbox::io::Reactor']]],
   ['hash_3c_20toolbox_3a_3aintwrapper_3c_20policyt_20_3e_20_3e_1',['hash&lt; toolbox::IntWrapper&lt; PolicyT &gt; &gt;',['../structstd_1_1hash_3_01toolbox_1_1IntWrapper_3_01PolicyT_01_4_01_4.html',1,'std']]],
-  ['hash_3c_20toolbox_3a_3aversion_20_3e_2',['hash&lt; toolbox::Version &gt;',['../structstd_1_1hash_3_01toolbox_1_1Version_01_4.html',1,'std']]],
-  ['help_3',['Help',['../classtoolbox_1_1util_1_1Help.html',1,'toolbox::Help'],['../classtoolbox_1_1util_1_1Help.html',1,'toolbox::util::Help']]],
-  ['histogram_4',['Histogram',['../classtoolbox_1_1hdr_1_1Histogram.html',1,'toolbox::hdr::Histogram'],['../classtoolbox_1_1hdr_1_1Histogram.html',1,'toolbox::Histogram']]],
-  ['hook_5',['Hook',['../structtoolbox_1_1io_1_1Hook.html',1,'toolbox::Hook'],['../structtoolbox_1_1io_1_1Hook.html',1,'toolbox::io::Hook']]]
+  ['hash_3c_20toolbox_3a_3autil_3a_3astringbuf_3c_20maxn_20_3e_20_3e_2',['hash&lt; toolbox::util::StringBuf&lt; MaxN &gt; &gt;',['../structstd_1_1hash_3_01toolbox_1_1util_1_1StringBuf_3_01MaxN_01_4_01_4.html',1,'std']]],
+  ['hash_3c_20toolbox_3a_3aversion_20_3e_3',['hash&lt; toolbox::Version &gt;',['../structstd_1_1hash_3_01toolbox_1_1Version_01_4.html',1,'std']]],
+  ['help_4',['Help',['../classtoolbox_1_1util_1_1Help.html',1,'toolbox::Help'],['../classtoolbox_1_1util_1_1Help.html',1,'toolbox::util::Help']]],
+  ['histogram_5',['Histogram',['../classtoolbox_1_1hdr_1_1Histogram.html',1,'toolbox::hdr::Histogram'],['../classtoolbox_1_1hdr_1_1Histogram.html',1,'toolbox::Histogram']]],
+  ['hook_6',['Hook',['../structtoolbox_1_1io_1_1Hook.html',1,'toolbox::Hook'],['../structtoolbox_1_1io_1_1Hook.html',1,'toolbox::io::Hook']]]
 ];
