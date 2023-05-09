72fd87190e2ddf097f6366b07cec7fde475c57d3
markaylett <markaylett@users.noreply.github.com>
2023-05-09 13:58:58 +0000

    deploy: fb14e201a5bcb3de65d9e930359e3ef52ec93148

--- search/namespaces_2.js
+++ search/namespaces_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['bm_0',['bm',['../namespacetoolbox_1_1bm.html',1,'toolbox']]],
-  ['detail_1',['detail',['../namespacetoolbox_1_1bm_1_1detail.html',1,'toolbox::bm::detail'],['../namespacetoolbox_1_1sys_1_1detail.html',1,'toolbox::detail'],['../namespacetoolbox_1_1sys_1_1detail.html',1,'toolbox::sys::detail'],['../namespacetoolbox_1_1util_1_1detail.html',1,'toolbox::util::detail']]],
+  ['detail_1',['detail',['../namespacetoolbox_1_1bm_1_1detail.html',1,'toolbox::bm::detail'],['../namespacetoolbox_1_1net_1_1detail.html',1,'toolbox::detail'],['../namespacetoolbox_1_1net_1_1detail.html',1,'toolbox::net::detail'],['../namespacetoolbox_1_1sys_1_1detail.html',1,'toolbox::sys::detail'],['../namespacetoolbox_1_1util_1_1detail.html',1,'toolbox::util::detail']]],
   ['hdr_2',['hdr',['../namespacetoolbox_1_1hdr.html',1,'toolbox']]],
   ['http_3',['http',['../namespacetoolbox_1_1http.html',1,'toolbox']]],
   ['io_4',['io',['../namespacetoolbox_1_1io.html',1,'toolbox']]],
