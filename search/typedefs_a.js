e57a723102afdb445224faad74ae9f031064e5c0
ItsLifeJim <ItsLifeJim@users.noreply.github.com>
2023-09-07 14:57:44 +0000

    deploy: 872e11b66d30cc5e6da9d547de926d62e16c5c7f

--- search/typedefs_a.js
+++ search/typedefs_a.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['micros_0',['Micros',['../namespacetoolbox_1_1sys.html#a51dccfb35d834e78588ead6c8d4cfc62',1,'toolbox::Micros()'],['../namespacetoolbox_1_1sys.html#a51dccfb35d834e78588ead6c8d4cfc62',1,'toolbox::sys::Micros()']]],
-  ['millis_1',['Millis',['../namespacetoolbox_1_1sys.html#a60e90ab2fc8417f81198604e8a291c46',1,'toolbox::Millis()'],['../namespacetoolbox_1_1sys.html#a60e90ab2fc8417f81198604e8a291c46',1,'toolbox::sys::Millis()']]],
-  ['monotime_2',['MonoTime',['../namespacetoolbox_1_1sys.html#a8370f3f6bd9eb78dd325ae3f4e582247',1,'toolbox::MonoTime()'],['../namespacetoolbox_1_1sys.html#a8370f3f6bd9eb78dd325ae3f4e582247',1,'toolbox::sys::MonoTime()']]],
-  ['mutablebuffer_3',['MutableBuffer',['../namespacetoolbox_1_1io.html#a5f27399855673bfa5dc5f79c77fa61c1',1,'toolbox::MutableBuffer()'],['../namespacetoolbox_1_1io.html#a5f27399855673bfa5dc5f79c77fa61c1',1,'toolbox::io::MutableBuffer()']]]
+  ['metriccallbackfunction_0',['MetricCallbackFunction',['../namespacetoolbox_1_1io.html#aefabfadce71f1cf5a6e140f7f8396cb9',1,'toolbox::MetricCallbackFunction()'],['../namespacetoolbox_1_1io.html#aefabfadce71f1cf5a6e140f7f8396cb9',1,'toolbox::io::MetricCallbackFunction()']]],
+  ['micros_1',['Micros',['../namespacetoolbox_1_1sys.html#a51dccfb35d834e78588ead6c8d4cfc62',1,'toolbox::Micros()'],['../namespacetoolbox_1_1sys.html#a51dccfb35d834e78588ead6c8d4cfc62',1,'toolbox::sys::Micros()']]],
+  ['millis_2',['Millis',['../namespacetoolbox_1_1sys.html#a60e90ab2fc8417f81198604e8a291c46',1,'toolbox::Millis()'],['../namespacetoolbox_1_1sys.html#a60e90ab2fc8417f81198604e8a291c46',1,'toolbox::sys::Millis()']]],
+  ['monotime_3',['MonoTime',['../namespacetoolbox_1_1sys.html#a8370f3f6bd9eb78dd325ae3f4e582247',1,'toolbox::MonoTime()'],['../namespacetoolbox_1_1sys.html#a8370f3f6bd9eb78dd325ae3f4e582247',1,'toolbox::sys::MonoTime()']]],
+  ['mutablebuffer_4',['MutableBuffer',['../namespacetoolbox_1_1io.html#a5f27399855673bfa5dc5f79c77fa61c1',1,'toolbox::MutableBuffer()'],['../namespacetoolbox_1_1io.html#a5f27399855673bfa5dc5f79c77fa61c1',1,'toolbox::io::MutableBuffer()']]]
 ];
