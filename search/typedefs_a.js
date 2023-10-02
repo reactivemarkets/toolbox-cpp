5a7a656e87499253eb7a0a5e2849de1ec9d1732a
markaylett <markaylett@users.noreply.github.com>
2023-10-02 12:41:31 +0000

    deploy: 3494390f7622f5874bac076ef73ad24d26bf76a8

--- search/typedefs_a.js
+++ search/typedefs_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['metriccallbackfunction_0',['MetricCallbackFunction',['../namespacetoolbox_1_1io.html#aefabfadce71f1cf5a6e140f7f8396cb9',1,'toolbox::MetricCallbackFunction()'],['../namespacetoolbox_1_1io.html#aefabfadce71f1cf5a6e140f7f8396cb9',1,'toolbox::io::MetricCallbackFunction()']]],
+  ['metriccallbackfunction_0',['MetricCallbackFunction',['../namespacetoolbox_1_1io.html#afb198a971ea0885273fe69800dcb5a82',1,'toolbox::MetricCallbackFunction()'],['../namespacetoolbox_1_1io.html#afb198a971ea0885273fe69800dcb5a82',1,'toolbox::io::MetricCallbackFunction()']]],
   ['micros_1',['Micros',['../namespacetoolbox_1_1sys.html#a51dccfb35d834e78588ead6c8d4cfc62',1,'toolbox::Micros()'],['../namespacetoolbox_1_1sys.html#a51dccfb35d834e78588ead6c8d4cfc62',1,'toolbox::sys::Micros()']]],
   ['millis_2',['Millis',['../namespacetoolbox_1_1sys.html#a60e90ab2fc8417f81198604e8a291c46',1,'toolbox::Millis()'],['../namespacetoolbox_1_1sys.html#a60e90ab2fc8417f81198604e8a291c46',1,'toolbox::sys::Millis()']]],
   ['monotime_3',['MonoTime',['../namespacetoolbox_1_1sys.html#a8370f3f6bd9eb78dd325ae3f4e582247',1,'toolbox::MonoTime()'],['../namespacetoolbox_1_1sys.html#a8370f3f6bd9eb78dd325ae3f4e582247',1,'toolbox::sys::MonoTime()']]],
--- search/variables_0.js
+++ search/variables_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['affinity_0',['affinity',['../structtoolbox_1_1sys_1_1ThreadConfig.html#afc7940a0d7333894c14b41c8083c4242',1,'toolbox::sys::ThreadConfig']]],
-  ['applicationjson_1',['ApplicationJson',['../namespacetoolbox_1_1http.html#a4535346f4eb5173c74ec6be3e63e9e46',1,'toolbox::ApplicationJson()'],['../namespacetoolbox_1_1http.html#a4535346f4eb5173c74ec6be3e63e9e46',1,'toolbox::http::ApplicationJson()']]]
+  ['app_5fname_1',['app_name',['../structtoolbox_1_1io_1_1MetricContext.html#a380617481539b5b1ad77db5a16e1422d',1,'toolbox::io::MetricContext']]],
+  ['applicationjson_2',['ApplicationJson',['../namespacetoolbox_1_1http.html#a4535346f4eb5173c74ec6be3e63e9e46',1,'toolbox::ApplicationJson()'],['../namespacetoolbox_1_1http.html#a4535346f4eb5173c74ec6be3e63e9e46',1,'toolbox::http::ApplicationJson()']]]
 ];
