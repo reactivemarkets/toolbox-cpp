8c451fe581db15fec05a381060d8595cfb75c3c5
markaylett <markaylett@users.noreply.github.com>
2024-02-23 13:18:51 +0000

    deploy: 61158bd6bfdacbcab192b6c4b51dfacf359d7d6c

--- search/typedefs_9.js
+++ search/typedefs_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['logmsgptr_0',['logmsgptr',['../namespacetoolbox_1_1sys.html#a90375ff3ad41e051d34219123f8cae1d',1,'toolbox::LogMsgPtr'],['../namespacetoolbox_1_1sys.html#a90375ff3ad41e051d34219123f8cae1d',1,'toolbox::sys::LogMsgPtr']]],
-  ['logstream_1',['logstream',['../namespacetoolbox_1_1sys.html#a2d316964302f8ef8f39a43db2a5992db',1,'toolbox::LogStream'],['../namespacetoolbox_1_1sys.html#a2d316964302f8ef8f39a43db2a5992db',1,'toolbox::sys::LogStream']]]
+  ['logstream_1',['logstream',['../namespacetoolbox_1_1sys.html#a2d316964302f8ef8f39a43db2a5992db',1,'toolbox::LogStream'],['../namespacetoolbox_1_1sys.html#a2d316964302f8ef8f39a43db2a5992db',1,'toolbox::sys::LogStream']]],
+  ['loopcallbackfunction_2',['loopcallbackfunction',['../namespacetoolbox_1_1io.html#a2196e8769618388a3b83b4f9195cc747',1,'toolbox::LoopCallbackFunction'],['../namespacetoolbox_1_1io.html#a2196e8769618388a3b83b4f9195cc747',1,'toolbox::io::LoopCallbackFunction']]]
 ];
