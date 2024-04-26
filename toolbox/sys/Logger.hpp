8b6f8073197f576c213caa2f657de77a84eed864
Mark Aylett <mark.aylett@reactivemarkets.com>
2024-04-26 16:16:32 +0100

    feat: Increase AsyncLogger queue size
    Increase AsyncLogger queue size to reduce the likelihood of drops.
    
    SDB-6988

--- toolbox/sys/Logger.hpp
+++ toolbox/sys/Logger.hpp
@@ -151,7 +151,7 @@ class TOOLBOX_API AsyncLogger : public Logger {
                       std::size_t size) noexcept override;
 
     Logger& logger_;
-    boost::lockfree::queue<Task, boost::lockfree::fixed_sized<true>> tq_{128};
+    boost::lockfree::queue<Task, boost::lockfree::fixed_sized<true>> tq_{512};
     std::atomic<bool> stop_{false};
 };
 
