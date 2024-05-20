a5c1e2cc81da3678093a3a96299c0d6640383590
jorgonlor <jorgonlor@users.noreply.github.com>
2024-05-20 16:50:09 +0000

    deploy: bb6a084589ab3d85442017dc061ec46f54f63418

--- search/typedefs_f.js
+++ search/typedefs_f.js
@@ -5,5 +5,6 @@ var searchData=
   ['size_5ftype_2',['size_type',['../classtoolbox_1_1util_1_1ArrayView.html#a97fbbbfe53b4f4b97009712d2c487f47',1,'toolbox::util::ArrayView']]],
   ['slot_3',['Slot',['../structtoolbox_1_1io_1_1Hook.html#af583f3f8d2105b0450cea6c72418a446',1,'toolbox::io::Hook']]],
   ['storageptr_4',['storageptr',['../namespacetoolbox_1_1util.html#ad09444d2adeb2a837168654823968f43',1,'toolbox::StoragePtr'],['../namespacetoolbox_1_1util.html#ad09444d2adeb2a837168654823968f43',1,'toolbox::util::StoragePtr']]],
-  ['streamendpoint_5',['streamendpoint',['../namespacetoolbox_1_1net.html#a85f1daa5df3c65656cf09655449408fc',1,'toolbox::StreamEndpoint'],['../namespacetoolbox_1_1net.html#a85f1daa5df3c65656cf09655449408fc',1,'toolbox::net::StreamEndpoint']]]
+  ['streamendpoint_5',['streamendpoint',['../namespacetoolbox_1_1net.html#a85f1daa5df3c65656cf09655449408fc',1,'toolbox::StreamEndpoint'],['../namespacetoolbox_1_1net.html#a85f1daa5df3c65656cf09655449408fc',1,'toolbox::net::StreamEndpoint']]],
+  ['sv_6',['sv',['../namespacetoolbox_1_1util.html#ab0e8390e06ed1ddfdfddf09408afb48e',1,'toolbox::sv'],['../namespacetoolbox_1_1util.html#ab0e8390e06ed1ddfdfddf09408afb48e',1,'toolbox::util::sv']]]
 ];
