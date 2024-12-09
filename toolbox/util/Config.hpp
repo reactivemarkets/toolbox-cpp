946be1e419dcf64e1cb41295c2467ef2d16cea39
Baber Nawaz <baber.nawaz@reactivemarkets.com>
2024-12-09 12:50:03 +0000

    chore: Refactor code with equivalent function call
    SDB-8261

--- toolbox/util/Config.hpp
+++ toolbox/util/Config.hpp
@@ -134,15 +134,7 @@ class TOOLBOX_API Config {
 
         const auto it{map_.find(key)};
         if (it != map_.end()) {
-            if constexpr (std::is_same_v<ValueT, std::string_view>) {
-                // Explicitly allow conversion to std::string_view in this case,
-                // because we know that the argument is not a temporary.
-                return std::string_view{it->second};
-            } else if constexpr (std::is_enum_v<ValueT>) {
-                return ValueT{from_string<std::underlying_type_t<ValueT>>(it->second)};
-            } else {
-                return from_string<ValueT>(it->second);
-            }
+            return transform_value<ValueT>(it->second);
         }
         if (!parent_) {
             throw std::runtime_error{std::string{"missing config key: "} + key};
