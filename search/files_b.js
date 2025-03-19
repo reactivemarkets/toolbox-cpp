4271a0c14464dc0eb1f6639af4e7201753e736c7
bnbajwa <bnbajwa@users.noreply.github.com>
2025-03-19 22:11:39 +0000

    deploy: c76c291a110e0c0d2e69714204384d6f0294bb1e

--- search/files_b.js
+++ search/files_b.js
@@ -3,5 +3,6 @@ var searchData=
   ['options_2ecpp_0',['Options.cpp',['../Options_8cpp.html',1,'']]],
   ['options_2ehpp_1',['Options.hpp',['../Options_8hpp.html',1,'']]],
   ['options_2eut_2ecpp_2',['Options.ut.cpp',['../Options_8ut_8cpp.html',1,'']]],
-  ['ostreambase_2ehpp_3',['OStreamBase.hpp',['../OStreamBase_8hpp.html',1,'']]]
+  ['ostreambase_2ecpp_3',['OStreamBase.cpp',['../OStreamBase_8cpp.html',1,'']]],
+  ['ostreambase_2ehpp_4',['OStreamBase.hpp',['../OStreamBase_8hpp.html',1,'']]]
 ];
