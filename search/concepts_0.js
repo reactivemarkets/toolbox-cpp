05b37a2166b2fc56d3c838e0701ae04c651fdde9
Shivix <Shivix@users.noreply.github.com>
2026-07-03 09:13:33 +0000

    deploy: 0f38792098e575bbb3d1c870cd4f584b6345dca7

--- search/concepts_0.js
+++ search/concepts_0.js
@@ -10,5 +10,6 @@ var searchData=
   ['toolbox_3a_3autil_3a_3aenum_7',['Enum',['../concepttoolbox_1_1util_1_1Enum.html',1,'toolbox::util']]],
   ['toolbox_3a_3autil_3a_3aisintwrapper_8',['IsIntWrapper',['../concepttoolbox_1_1util_1_1IsIntWrapper.html',1,'toolbox::util']]],
   ['toolbox_3a_3autil_3a_3apointer_9',['Pointer',['../concepttoolbox_1_1util_1_1Pointer.html',1,'toolbox::util']]],
-  ['toolbox_3a_3autil_3a_3astreamable_10',['Streamable',['../concepttoolbox_1_1util_1_1Streamable.html',1,'toolbox::util']]]
+  ['toolbox_3a_3autil_3a_3astreamable_10',['Streamable',['../concepttoolbox_1_1util_1_1Streamable.html',1,'toolbox::util']]],
+  ['toolbox_3a_3autil_3a_3awiremappable_11',['WireMappable',['../concepttoolbox_1_1util_1_1WireMappable.html',1,'toolbox::util']]]
 ];
